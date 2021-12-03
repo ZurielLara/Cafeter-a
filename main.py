451fcb0cbb1b0f96ddbbd18cfbe57dd3be969ea9
unknown <n00n3fs@gmail.com>
2021-12-03 02:10:08 -0600

    Primera version de la interfaz

--- main.py
+++ main.py
@@ -1,23 +1,128 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+from PyQt6.QtWidgets import *
 import sys
 
-class Ventana(QWidget):
+
+class Window(QMainWindow):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
+
+        # set the title of main window
         self.setWindowTitle('Cafeteria')
-        self.showMaximized()
+
+        # set the size of window
+        self.Width = 800
+        self.height = int(0.618 * self.Width)
+        self.resize(self.Width, self.height)
+		
+		# add all widgets
+        self.btn_1 = QPushButton('Pedido', self)
+        self.btn_2 = QPushButton('Inventario', self)
+        self.btn_3 = QPushButton('Ventas', self)
+
+        # Hoja de estilos
+        self.btn_1.setStyleSheet('border: none;')
+        self.btn_2.setStyleSheet('border: none;')
+        self.btn_3.setStyleSheet('border: none;')
+
+        self.btn_1.clicked.connect(self.button1)
+        self.btn_2.clicked.connect(self.button2)
+        self.btn_3.clicked.connect(self.button3)
+
+        # add tabs
+        self.tab1 = self.ui1()
+        self.tab2 = self.ui2()
+        self.tab3 = self.ui3()
+
+        self.initUI()
+
+    def initUI(self):
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.btn_1)
+        left_layout.addWidget(self.btn_2)
+        left_layout.addWidget(self.btn_3)
+        left_layout.addStretch(5)
+        left_layout.setSpacing(20)
+        left_widget = QWidget()
+        left_widget.setStyleSheet("background-color: brown;")
+        left_widget.setLayout(left_layout)
+
+        self.right_widget = QTabWidget()
+        self.right_widget.tabBar().setObjectName("mainTab")
+
+        self.right_widget.addTab(self.tab1, '')
+        self.right_widget.addTab(self.tab2, '')
+        self.right_widget.addTab(self.tab3, '')
+
+        self.right_widget.setCurrentIndex(0)
+        self.right_widget.setStyleSheet('''QTabBar::tab{width: 0; \
+            height: 0; margin: 0; padding: 0; border: none; background-color: red}''')
+
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(left_widget)
+        main_layout.addWidget(self.right_widget)
+        main_layout.setStretch(0, 40)
+        main_layout.setStretch(1, 200)
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+
+    # Botones
+
+    def button1(self):
+        self.right_widget.setCurrentIndex(0)
+        self.btn_1.setStyleSheet('background-color: red; border: none;')
+        self.btn_2.setStyleSheet('background-color: brown; border: none;')
+        self.btn_3.setStyleSheet('background-color: brown; border: none;')
+
+    def button2(self):
+        self.right_widget.setCurrentIndex(1)
+        self.btn_1.setStyleSheet('background-color: brown; border: none;')
+        self.btn_2.setStyleSheet('background-color: red; border: none;')
+        self.btn_3.setStyleSheet('background-color: brown; border: none;')
+
+    def button3(self):
+        self.right_widget.setCurrentIndex(2)
+        self.btn_1.setStyleSheet('background-color: brown; border: none;')
+        self.btn_2.setStyleSheet('background-color: brown; border: none;')
+        self.btn_3.setStyleSheet('background-color: red; border: none;')
+	
+# Páginas
+
+    def ui1(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 1'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
+
+    def ui2(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 2'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
         
-        boton1 = QPushButton('Finalizar Pedido')
-        boton1.setFixedSize(100, 50)
+    def ui3(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 3'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
 
-        grid = QGridLayout()
-        grid.addWidget(boton1, 10, 10)
-        self.setLayout(grid)
+    def ui4(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 4'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
 
 
-app = QApplication(sys.argv)
-
-ventana = Ventana()
-ventana.show()
-
-sys.exit(app.exec())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec())
